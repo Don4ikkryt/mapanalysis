2d46ce093e006a4dceb60e34d9635b7a867704f4
Don4ikkryt <don4ikkryt@gmail.com>
2019-10-31 22:15:56 +0200

    push

--- mapAnalysis.go
+++ mapAnalysis.go
@@ -1,27 +1,23 @@
 package main
 
-import(
-	"fmt"
+import (
 	"flag"
-
-	"github.com/jung-kurt/gofpdf"
 )
 
 var (
-	sourceFolder   string
-	filteredFolder string
-	maxDistanceBetweenPoints int16
+	sourceFolder             string
+	filteredFolder           string
+	maxDistanceBetweenPoints int64
 )
 
-func main(){
+func main() {
 	parseFlags()
-
 
 }
 func parseFlags() {
 	flag.StringVar(&sourceFolder, "source_folder", "", "Path to the folder with photos")
 	flag.StringVar(&filteredFolder, "filtered_folder", "", "Path to the folder with filtered (unsupported format/no exif data) files")
-	flag.Int(&maxDistanceBetweenPoints, "max_distance_between_points", 0, "Maximum allowed distance between two points, where two photoes were taken")
-	
+	flag.Int64Var(&maxDistanceBetweenPoints, "max_distance_between_points", 0, "Maximum allowed distance between two points, where two photoes were taken")
+
 	flag.Parse()
 }
