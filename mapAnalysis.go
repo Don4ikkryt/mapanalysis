5ae3e1f20c29d8b50da643ffaef9f737aed6cd69
Don4ikkryt <don4ikkryt@gmail.com>
2019-11-03 19:49:51 +0200

    almost created pdfMap struct

--- mapAnalysis.go
+++ mapAnalysis.go
@@ -13,8 +13,8 @@ var (
 	maxDistanceBetweenPoints int64
 )
 
-const maxLenght int16 = 200
-const maxWidth int16 = 200
+const maxLenght int16 = 205
+const maxWidth int16 = 205
 
 type pdfMap struct {
 	points    []readcoordinates.Point
@@ -23,15 +23,18 @@ type pdfMap struct {
 	lenght    int16
 }
 
-func newPoint(points []readcoordinates.Point, propotion float64) *pdfMap {
+func newMap(points []readcoordinates.Point, propotion float64) *pdfMap {
 	m := pdfMap{propotion: propotion, points: points, lenght: maxLenght}
 	return &m
 }
-func (m *pdfMap) defineWidth() {
-	m.width = int16(float64(m.lenght)/m.propotion) + 2
+func (m *pdfMap) defineWidthAndLenght() {
+	m.width = int16(float64(m.lenght-5)/m.propotion) + 5
 	if m.width > maxWidth {
-		m.width = int16(float64(m.width)*m.propotion) + 1
-
+		m.width = int16(float64(m.width)*m.propotion) + 5
+		m.lenght = int16(float64(m.width-5)*m.propotion) + 5
+		m.lenght = toOdd(m.lenght)
+		m.width = toOdd(m.width)
+		return
 	} else {
 		return
 	}
@@ -41,10 +44,12 @@ func main() {
 	parseFlags()
 	points, propotion := readcoordinates.GetCoordinatesAndPropotion(sourceFolder, filteredFolder)
 
-	fmt.Println(propotion)
-	for _, value := range points {
-		fmt.Println(value)
-	}
+	map1 := newMap(points, propotion)
+	map1.defineWidthAndLenght()
+	fmt.Println(map1.propotion)
+	fmt.Println(map1.lenght)
+	fmt.Println(map1.width)
+	readcoordinates.
 }
 func parseFlags() {
 	flag.StringVar(&sourceFolder, "source_folder", "", "Path to the folder with photos")
@@ -52,4 +57,11 @@ func parseFlags() {
 	flag.Int64Var(&maxDistanceBetweenPoints, "max_distance_between_points", 0, "Maximum allowed distance between two points, where two photoes were taken")
 
 	flag.Parse()
+}
+func toOdd(number int16) int16 {
+	if number%2 == 1 {
+		return number
+	} else {
+		return number + 1
+	}
 }
