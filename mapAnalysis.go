f35c5d107cba48288d2b21bffd27ad47fff7799c
Don4ikkryt <don4ikkryt@gmail.com>
2019-11-03 20:36:39 +0200

    nothing special

--- mapAnalysis.go
+++ mapAnalysis.go
@@ -3,18 +3,21 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/Don4ikkryt/readcoordinates"
+	"github.com/jung-kurt/gofpdf"
 )
 
 var (
 	sourceFolder             string
+	mapFolder                string
 	filteredFolder           string
 	maxDistanceBetweenPoints int64
 )
 
-const maxLenght int16 = 205
-const maxWidth int16 = 205
+const maxLenght int16 = 200
+const maxWidth int16 = 200
 
 type pdfMap struct {
 	points    []readcoordinates.Point
@@ -28,16 +31,16 @@ func newMap(points []readcoordinates.Point, propotion float64) *pdfMap {
 	return &m
 }
 func (m *pdfMap) defineWidthAndLenght() {
-	m.width = int16(float64(m.lenght-5)/m.propotion) + 5
+	m.width = int16(float64(m.lenght) / m.propotion)
 	if m.width > maxWidth {
-		m.width = int16(float64(m.width)*m.propotion) + 5
-		m.lenght = int16(float64(m.width-5)*m.propotion) + 5
-		m.lenght = toOdd(m.lenght)
-		m.width = toOdd(m.width)
-		return
-	} else {
+		m.width = int16(float64(m.width) * m.propotion)
+		m.lenght = int16(float64(m.width) * m.propotion)
+
 		return
 	}
+	m.lenght = toOdd(m.lenght)
+	m.width = toOdd(m.width)
+	return
 
 }
 func main() {
@@ -46,15 +49,13 @@ func main() {
 
 	map1 := newMap(points, propotion)
 	map1.defineWidthAndLenght()
-	fmt.Println(map1.propotion)
-	fmt.Println(map1.lenght)
-	fmt.Println(map1.width)
-	readcoordinates.
+
 }
 func parseFlags() {
 	flag.StringVar(&sourceFolder, "source_folder", "", "Path to the folder with photos")
 	flag.StringVar(&filteredFolder, "filtered_folder", "", "Path to the folder with filtered (unsupported format/no exif data) files")
 	flag.Int64Var(&maxDistanceBetweenPoints, "max_distance_between_points", 0, "Maximum allowed distance between two points, where two photoes were taken")
+	flag.StringVar(&mapFolder, "map_folder", "", "Folder where PDF file will be created")
 
 	flag.Parse()
 }
@@ -63,5 +64,20 @@ func toOdd(number int16) int16 {
 		return number
 	} else {
 		return number + 1
+	}
+}
+
+func createPDFFile(f *gofpdf.Fpdf) {
+	var i int = 1
+	var filename string = "map"
+	for {
+		if _, err := os.Stat(mapFolder + "\\" + filename + string(i)); os.IsNotExist(err) {
+			break
+		}
+		i++
+	}
+	err := f.OutputFileAndClose(mapFolder + "\\" + filename + string(i))
+	if err != nil {
+		fmt.Println("err")
 	}
 }
